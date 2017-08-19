dabba893ca564ee87a9ea2a25a9e57a2040dae7d
Jailani Abdul Rahman <jailani.har@googlemail.com>
2017-08-19 12:00:33 +0800

    Added forloop, nested loop, break and continue statement source code practical

---
+++ src/week6/LearningForLoop.java
@@ -0,0 +1,24 @@
+package week6;
+
+import java.util.Scanner;
+
+public class LearningForLoop {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input value:");
+		int userInput = scanner.nextInt();
+		// for(initial-action; loop-continuation-condition; action-after-iteration) {}
+		for(int a = 0; a < userInput; a++) {
+			System.out.println(a + ": Welcome to Java");
+		}
+		
+		int b;
+		for(b = 0; b < userInput; b++) {
+			System.out.println(b + ": Programming Application");
+		}
+		
+		scanner.close();
+	}
+	
+}
