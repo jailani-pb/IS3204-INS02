c3875377f5040f0c7f1a912125275b2cbda63018
Jailani Abdul Rahman <jailani.har@googlemail.com>
2017-09-28 08:10:17 +0800

    Added if the word does not exist it returns -1

--- src/week11/LearningRetrieveIndexChar.java
+++ src/week11/LearningRetrieveIndexChar.java
@@ -3,7 +3,7 @@ package week11;
 public class LearningRetrieveIndexChar {
 
 	public static void main(String[] args) {
-		String message = "Programming Application";
+		String message = "Programming Java Application";
 		// Retrieve the index of first instance of letter a
 		int firstInstanceA = message.indexOf("a");
 		System.out.println(firstInstanceA);
@@ -14,6 +14,20 @@ public class LearningRetrieveIndexChar {
 		String subMessage =
 				message.substring(firstInstanceA, lastInstanceA+1);
 		System.out.println(subMessage);
+		
+		int firstInstanceJava = message.indexOf("Java");
+		System.out.println(firstInstanceJava);
+		
+		if(firstInstanceJava == -1) {
+			System.out.println("Java is not in the word " 
+					+ message);
+		} else {
+			System.out.println("Java is in the word " + message);
+			System.out.println("In index: " + firstInstanceJava);
+		}
 	}
 	
 }
+
+
+
